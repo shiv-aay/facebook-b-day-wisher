21a73edfddd13aa4a9eb8b490d8c471a5bdce31f
Shivaay <58257395+shiv-aay@users.noreply.github.com>
2021-07-06 01:37:41 +0530

    Added several new features; Default browser changed to Chrome
    Input taking from CLI
    "Send now" is now optional
    Elements are now being found by tag_name rather than class_name except one
    Notifications disabled
    try/except added inside for loop

--- facebook_automatic_birthday_wish.py
+++ facebook_automatic_birthday_wish.py
@@ -3,40 +3,68 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from getpass import getpass
 from time import sleep
 
-# Install your browser's driver and put its path in the following line. 
-# Here I've used Edge Browser which is Chromium based, Chromium can also be used here
-## https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
-driver=webdriver.Edge(r"C:\Users\pc\Desktop\msedgedriver.exe") # put the path of your driver in the double quotes
-driver.get('https://facebook.com/')
-driver.maximize_window()
+# Install your browser's driver and put its path in the following line.
+driver_address = r"C:\Users\pc\Desktop\chromedriver.exe"
+# Here I've used Chrome Browser which is Chromium based.
+# hence this script also supports Edge browser with a slight modification
 
-## Logging in
-WebDriverWait(driver,30).until(EC.presence_of_element_located((By.ID, 'email')))
-username_type = driver.find_element_by_xpath("//input[@name=\"email\"]")
-username_type.send_keys("example@gmail.com")#Enter your username in double quotes
-password_type = driver.find_element_by_xpath("//input[@name=\"pass\"]")
-password_type.send_keys("example_password")#Enter your password in double quotes
-log_in = driver.find_element_by_xpath("//button[@type='submit']")
-log_in.click()
-## Logged in
-sleep(2)
-## Going to Birthday's Page
-driver.get('https://facebook.com/events/birthdays/')
-WebDriverWait(driver,100).until(EC.presence_of_element_located((By.CSS_SELECTOR, "._5rpu")))
-try:
-    todays = driver.find_element_by_class_name('sjgh65i0')
-    wish_forms = todays.find_elements_by_class_name('_5rpu')
-    # todays = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[1]")
-    # wish_forms = todays.find_elements_by_xpath("/div/div/div/div[2]/div[1]/div/div[2]/div[2]/div/form/div/div/div[1]/div/div/div[2]/div")
-    names = todays.find_elements_by_class_name('cbu4d94t')
-    for wish_form,name in zip(wish_forms,names[1:]):
-        msg="Happy Birthday "+name.text.split()[0]
-        wish_form.send_keys(msg)
-        # Uncomment the next line to automatically send the wishes
-        ## wish_form.send_keys(Keys.ENTER)
-except Exception as e:
-        print(str(e))
+def start(username , password , send_now):
+    # incoming notifications during the process causes problems
+    # following parameter disables browser notifications
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-notifications")
+    driver=webdriver.Chrome(driver_address , options=chrome_options) # put the path of your driver in the double quotes
+    driver.get('https://facebook.com/')
+    driver.maximize_window()
 
-## For any queries or feedback, feel free to contact me here shivoydixit@outlook.com
+    ## Logging in
+    WebDriverWait(driver,30).until(EC.presence_of_element_located((By.ID, 'email')))
+    username_type = driver.find_element_by_xpath("//input[@name=\"email\"]")
+    username_type.send_keys(username)
+    password_type = driver.find_element_by_xpath("//input[@name=\"pass\"]")
+    password_type.send_keys(password)
+    log_in = driver.find_element_by_xpath("//button[@type='submit']")
+    log_in.click()
+    ## Logged in
+    sleep(2)
+    ## Going to Birthday's Page
+    driver.get('https://facebook.com/events/birthdays/')
+    WebDriverWait(driver,100).until(EC.presence_of_element_located((By.CSS_SELECTOR, "._5rpu")))
+    try:
+        # class 'discj3wi' may change after some time, but class of any element
+        # which contains list of all friend's who have birthday today would work
+        today = driver.find_elements_by_class_name('discj3wi')
+        todays = today[1]
+
+        # inner most div element in form
+        wish_forms = todays.find_elements_by_xpath("//div[@data-block='true']")
+        names = todays.find_elements_by_tag_name('h2')
+
+        send = False
+        if(send_now[0] == 'y' or send_now[0] == 'Y'): send = True
+
+        # names[0] is 'Recent birthdays'
+        for wish_form,name in zip(wish_forms,names[1:]):
+            try:
+                name_of_friend = name.find_element_by_tag_name('span').text.split()[0]
+                msg="Happy Birthday " + name_of_friend
+                wish_form.find_element_by_tag_name('div').send_keys(msg)
+                if(send) : wish_form.send_keys(Keys.ENTER)
+            except:
+                print("Exception occured for 1 friend")
+    except Exception as e:        
+            print(str(e))
+    input()
+
+# taking inputs
+username = input("Enter your facebook email/phone number : ")
+send_now = input("Do you want to post wishes instantaneously ? ")
+password = getpass()
+# start the process
+start(username = username , password = password , send_now = send_now)
+
+## For any queries or feedback, feel free to contact me here shivoy4ndixit@gmail.com
